9048c64f5cb4f706f68bfdafbb701937301e0d15
aalyman42 <aalyman42@gmail.com>
2024-03-13 11:53:34 -0600

    refactor file structure for expo

--- SummitQuest/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import "../App.css";
-export default function basicView() {
-  return (
-    <h1 className="montserrat-title">
-      Default view to check outlet functionality
-    </h1>
-  );
-}
--- SummitQuest/src/pages/Profile.jsx
+++
@@ -1,3 +0,0 @@
-export default function basicView() {
-  return <h1>Default view to check outlet functionality</h1>;
-}
